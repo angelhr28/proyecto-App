f07a6a4a6e4f63f337493940a4777d179682bdc2
angelozohe22 <luis_angelo21@hotmail.com>
2020-09-10 01:19:36 -0500

    changes 0.2

--- app/src/main/java/com/example/proyectsad/modules/login/view/RegisterActivity.kt
+++ app/src/main/java/com/example/proyectsad/modules/login/view/RegisterActivity.kt
@@ -3,12 +3,16 @@ package com.example.proyectsad.modules.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.proyectsad.R
+import com.example.proyectsad.helper.aplication.setColorToNavigatioBar
+import com.example.proyectsad.helper.aplication.setColorToStatusBar
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        setColorToStatusBar(this)
+        setColorToNavigatioBar(this)
     }
 
 
