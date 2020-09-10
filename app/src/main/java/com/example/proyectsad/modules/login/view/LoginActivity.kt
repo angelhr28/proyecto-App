73e5542be5d61d3b9a6598f754fb89a9a568d519
angel <angelhuamannahui@gmail.com>
2020-09-09 22:32:50 -0500

    holi

--- app/src/main/java/com/example/proyectsad/modules/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/proyectsad/modules/login/view/LoginActivity.kt
@@ -3,12 +3,15 @@ package com.example.proyectsad.modules.login.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.proyectsad.R
+import com.example.proyectsad.helper.aplication.setColorToNavigatioBar
+import com.example.proyectsad.helper.aplication.setColorToStatusBar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        setColorToStatusBar(this)
+        setColorToNavigatioBar(this)
 
     }
 }
