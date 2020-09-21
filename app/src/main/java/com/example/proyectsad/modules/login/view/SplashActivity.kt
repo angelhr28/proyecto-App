f7af53592fc0a15d9ed3d541248242a8144b8365
angelozohe22 <luis_angelo21@hotmail.com>
2020-09-21 13:26:29 -0500

    a

--- app/src/main/java/com/example/proyectsad/modules/login/view/SplashActivity.kt
+++ app/src/main/java/com/example/proyectsad/modules/login/view/SplashActivity.kt
@@ -58,12 +58,6 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
-//        runnable?.let {
-//            handler?.postDelayed(it,SPLASH_DELAY)
-//        }
-//        @PENDIENTE PARA QUE PONES UN LET DE ALGO QUE DE POR SI NO TE PIDEN ??   EL LET NO DEBERIA SER DEL RUNNABLE ?? ?  ALGO ASI >:V
-
         handler?.let {
             it.postDelayed(runnable,SPLASH_DELAY)
         }
@@ -71,8 +65,8 @@ class SplashActivity : AppCompatActivity() {
 
     private fun getRunnable():Runnable?{
         return Runnable {
-            Thread {// @ PENDIENTE QUE PARTE DE NO QUIERO VER NINGUN !! EN LA APP NO SE ENTENDIO  >:V   PONLE UNA FUNCION TERNARIOA EJEMPLO
-                while (progressBarStatus ?: 0 < 50) {    // PONLO ASI  while (progressBarStatus ?: 0 < 50) {
+            Thread {
+                while (progressBarStatus ?: 0 < 50) {
                     try {
                         dummy = dummy?.plus(1)
                         Thread.sleep(50)
@@ -109,12 +103,6 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-//        runnable?.let {
-//            handler?.removeCallbacks(it)   INVESTIGA COMO USAR EL PTO LET
-//        }
-//        @PENDIENTE PARA QUE PONES UN LET DE ALGO QUE DE POR SI NO TE PIDEN ??   EL LET NO DEBERIA SER DEL RUNNABLE ?? ?  ALGO ASI >:V
-//        LA MISMA WEBADA ACA
-
         handler?.let {
             it.removeCallbacks(runnable)
         }
